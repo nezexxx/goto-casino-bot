37ba6f2d44fb5be1886f25cc649c0d046ce4b816
ivan <tretyakov_in@mail.ru>
2021-08-08 18:23:30 +0300

    update slots.py

--- locations/roulette.py
+++ locations/roulette.py
@@ -7,4 +7,5 @@ def process_message(message, user, users, bot, location_manager):
     if message.text == "Ставка":
         bot.send_message(user['id'], "Вы делаете ставку")
     else:
-        bot.send_message(user['id'], "Вы в рулетке.Чтобы начать иру,вам над поставить ставку.Для этого вам надо написать 'Ставка'")
+        bot.send_message(user['id'], """Вы в рулетке.Чтобы начать игру,
+        вам над поставить ставку.Для этого вам надо написать 'Ставка'""")
--- locations/slots.py
+++ locations/slots.py
@@ -6,9 +6,10 @@ from telebot import types
 
 options = list('🍒🍋🍐')
 
+stav_50 = 0
 stav_100 = 0
 stav_200 = 0
-
+stav_500= 0
 
 def animate(chat_id, message_id, bot, result):
 	time.sleep(0.5)
@@ -29,10 +30,15 @@ def animate(chat_id, message_id, bot, result):
 
 def send_menu(chat_id, bot):
 	keyboard = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+	stav_50 = types.KeyboardButton(text="💸 50")
+	keyboard.add(stav_50)
 	stav_100 = types.KeyboardButton(text="💸 100")
 	keyboard.add(stav_100)
 	stav_200 = types.KeyboardButton(text="💸 200")
 	keyboard.add(stav_200)
+	stav_500 = types.KeyboardButton(text="💸 500")
+	keyboard.add(stav_500)
+
 	keyboard.add(types.KeyboardButton(text="❌ Меню"))
 
 	bot.send_message(chat_id, "Сделайте ставку", reply_markup=keyboard)
@@ -43,23 +49,42 @@ def run_animation(chat_id, result, bot):
 	t = Thread(target=animate, args=(chat_id, message.id, bot, result))
 	t.start()
 
+def raschet(user, users, bot):
+	combination = random.choice(options) + random.choice(options) + random.choice(options)
+	run_animation(user['id'], combination, bot)
 
+	if combination in ['🍒🍒🍒', '🍋🍋🍋', '🍐🍐🍐']:
+		bot.send_message(user['id'], "Вы выиграли")
+		user['balance'] += stav_50 * 1.3
+		user['balance'] += stav_100 * 1.3
+		user['balance'] += stav_200 * 1.3
+		user['balance'] += stav_500 * 1.3
+	else:
+		bot.send_message(user['id'], "ВЫ проиграли")
+		user['balance'] -= stav_50
+		user['balance'] -= stav_100
+		user['balance'] -= stav_200
+		user['balance'] -= stav_500
 def process_message(message, user, users, bot):
-	if "100" in message.text:
+	if "50" in message.text:
+		if user['balance'] < 50:
+			bot.send_message(user['id'], 'Недостаточно средств на балансе')
+		else:
+			raschet(user, users, bot)
+	elif "100" in message.text:
 		if user['balance'] < 100:
 			bot.send_message(user['id'], 'Недостаточно средств на балансе')
 		else:
-			combination = random.choice(options) + random.choice(options) + random.choice(options)
-			run_animation(user['id'], combination, bot)
-
-			if combination in ['🍒🍒🍒', '🍋🍋🍋', '🍐🍐🍐']:
-				bot.send_message(user['id'], "Вы выиграли")
-				user['balance'] += stav_100 * 1.3
-				user['balance'] += stav_200 * 1.3
-			else:
-				bot.send_message(user['id'], "ВЫ проиграли")
-				user['balance'] -= stav_100
-				user['balance'] -= stav_200
-
+			raschet(user, users, bot)
+	elif "200" in message.text:
+		if user['balance'] < 200:
+			bot.send_message(user['id'], 'Недостаточно средств на балансе')
+		else:
+			raschet(user, users, bot)
+	elif "200" in message.text:
+		if user['balance'] < 200:
+			bot.send_message(user['id'], 'Недостаточно средств на балансе')
+		else:
+			raschet(user, users, bot)
 	else:
 		send_menu(user['id'], bot)
