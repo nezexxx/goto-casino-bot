fe9eddf7bcc3cf6990ace1cc0e6fbcb515f21d8c
aleksejaroslavskij <leha.yaroslavskiy@list.ru>
2021-08-09 21:35:07 +0300

    fix rebase after vova

--- locations/balance.py
+++ locations/balance.py
@@ -1,20 +1,27 @@
-from helpers import generate_keyboard
-else:
-bot.send_message(user, answer["balance"])
-keyboard = generate_keyboard(["balance"])
-host = 'https://bank.goto.msk.ru'
-trading_token = token
+import requests
+from config import token
+'''  host = 'https://bank.goto.msk.ru'
+trading_token = token '''
+from helpers import generate_keyboard  
 
+def change_location(user,bot,message):
+    user["id"] = message.chat.id
+    keyboard = generate_keyboard(["Пополнить баланс", "❌ Меню"])
+    bot.send_message(user["id"],user["balance"], reply_markup=keyboard)
+    '''
+    def process_message(message, user, users, bot, location_manager,from_id, amount, description):
+      if "Пополнить баланс" in message.text:
+          bot.send_message(user_id, "Введите сумму,котрую хотите положить на баланс")
+          answer = requests.post(host + '/api/ask', json={
+              'token': trading_token,
+              'account_id': from_id,
+              'amount': amount,
+              'description': description
+                         
 
-def ask_money(from_id, amount, description):
-    bot.send_message(user,)
-    answer = requests.post(host + '/api/ask', json={
-        'token': trading_token,
-        'account_id': from_id,
-        'amount': amount,
-        'description': description
+          return answer.json()
 
-    return answer.json()
+          ask_money(user, bet, "Закинуть в казино")
 
-
-ask_money(user['id'],answer, "ОПИСАНИЕ ПОКУПКИ" )
+      elif "Меню" in message.text:
+          location_manager.change_location(user, "menu", users, bot, location_manager)   '''
